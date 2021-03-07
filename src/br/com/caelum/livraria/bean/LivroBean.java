ae3e73aaf3298b22d2cecb23e1bafb744ae281d7
felipecarvalhodesouza <desouzafelipecarvalho@gmail.com>
2021-03-07 14:20:11 -0300

    Atualizando projeto com JPA

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -2,20 +2,26 @@ package br.com.caelum.livraria.bean;
 
 import javax.faces.bean.ManagedBean;
 
+import br.com.caelum.livraria.dao.DAO;
+import br.com.caelum.livraria.modelo.Livro;
+
 @ManagedBean
 public class LivroBean {
 
 	private Livro livro = new Livro();
 
-	public void gravar() {
-		System.out.println("Gravando livro " + this.getLivro().getTitulo());
-	}
-
 	public Livro getLivro() {
 		return livro;
 	}
 
-	public void setLivro(Livro livro) {
-		this.livro = livro;
+	public void gravar() {
+		System.out.println("Gravando livro " + this.livro.getTitulo());
+
+		if (livro.getAutores().isEmpty()) {
+			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+		}
+
+		new DAO<Livro>(Livro.class).adiciona(this.livro);
 	}
+
 }
